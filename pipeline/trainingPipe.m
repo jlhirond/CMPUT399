e686f212b3d9678223bd9c80228c3a9692fad230
kdshapka <kshapka@ualberta.ca>
2014-11-27 16:40:36 -0700

    randomMatrix

--- pipeline/trainingPipe.m
+++ pipeline/trainingPipe.m
@@ -62,6 +62,7 @@ toc
 
 % randomly rearrange images so that all the positive images are not contained within one fold
 % and/or choose smaller number of negative images
+% rearranging a matrix: shuffledArray = orderedArray(randperm(size(orderedArray,1)),:);
 
 % perform cross validation in order to find the optimal value for our
 % tuning parameter(s).
